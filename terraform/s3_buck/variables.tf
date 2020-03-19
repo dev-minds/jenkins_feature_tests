a16c9683144a86611b2dac05240b72cc2445ab8a
fmbah <felix.mbah@matchesfashion.com>
2020-03-19 15:29:32 +0000

    Fixing

--- terraform/s3_buck/variables.tf
+++ terraform/s3_buck/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "Specify unique bucket name. If ommitted, a random name will be generated"
-  type        = string
-  default     = ""
+  type        = list
+  default     = []
 }
 
 variable "region" {
